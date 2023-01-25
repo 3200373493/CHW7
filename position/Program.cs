d3e47dfaff710c6f644baf8df3c5b69b6d2bbbd1
sergey <fedulov_sy@mail.ru>
2023-01-25 03:15:16 +0300

    Made Homework

---
+++ position/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 100);
+PrintArray(array);
+SearchPosition(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SearchPosition(int[,] array)
+{
+    Console.Write("Введите порядковый номер строки = ");
+    int row = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите порядковый номер столбца = ");
+    int column = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == row && j == column)
+            {
+                Console.Write($"Значение искомого элемента = {array[i - 1, j - 1]}");
+            }
+
+        }
+    }
+    if (row > array.GetLength(0) || column > array.GetLength(1)) { Console.Write("Такого элемента нет (("); }
+
+}
+
+
+
